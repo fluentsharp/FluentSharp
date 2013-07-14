cd2be8d43d5650af4ce5aa870add6a0ee492179c
Dinis Cruz <dinis.cruz@owasp.org>
2013-07-14 02:29:51 +0100

    Updated NuSpec files Added UnitTests project for FluentSharp.WPF (with one test) Added read_Response extension methods to NetWork_ExtensionMethods.cs

--- FluentSharp.CoreLib/ExtensionMethods/NetWork_ExtensionMethods.cs
+++ FluentSharp.CoreLib/ExtensionMethods/NetWork_ExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
@@ -95,8 +96,35 @@ namespace FluentSharp.CoreLib
                 }
             return null;
         }
+        public static byte[]        read_Response_Bytes(this TcpClient tcpClient)
+        {
+            if(tcpClient.isNotNull())
+            {
+                try
+                {                  
+                    var memoryStream = new MemoryStream();
+                    var bytes = new byte[1024];
+                    var bytesRead = tcpClient.stream().Read(bytes, 0, bytes.size());
+                    while(bytesRead > 0)
+                    {
+                        memoryStream.Write(bytes,0,bytesRead);    
+                        bytesRead = tcpClient.stream().Read(bytes, 0, bytes.size());
+                    }                    
+                    return memoryStream.ToArray();
+                }
+                catch(Exception ex)
+                {
+                    ex.log("[TcpClient][readResponseBytes]");   
+                }
+            }
+            return new byte[0];
+        }
+        public static string        read_Response(this TcpClient tcpClient)
+        {
+            return tcpClient.read_Response_Bytes().ascii();
+        }
 
-        //TcpListener
+        //Socket
         public static byte[]        getAvailableBytes(this Socket socket)
         {            
             if (socket.notNull())
@@ -113,6 +141,7 @@ namespace FluentSharp.CoreLib
             return new byte[0];
         }
 
+        //TcpListener
         public static TcpListener   tcpListener(this int localPort)
         {
             try
@@ -125,8 +154,7 @@ namespace FluentSharp.CoreLib
                 ex.log("[TcpListener]");
                 return null;
             }
-        }
-        
+        }        
         public static TcpListener   start             (this TcpListener tcpListener)
         {
             if (tcpListener.notNull())
---
+++ UnitTests/UnitTests.FluentSharp.WPF/Test_WinFormsIntegration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentSharp.CoreLib;
+using FluentSharp.WPF;
+using FluentSharp.WinForms;
+using NUnit.Framework;
+
+namespace UnitTests.FluentSharp.WPF
+{
+    [TestFixture]
+    public class Test_WinFormsIntegration
+    {
+        [Test]
+        public void add_Wpf()
+        {
+            var winFormControl = "popupWindow".popupWindow(); // returns a System.Windows.Forms.Panel
+            var elementHost = winFormControl.add_Wpf();
+
+            Assert.IsNotNull(winFormControl);
+            Assert.IsNotNull(elementHost);
+            
+            
+            var wpfGrid = elementHost.add_Grid_Wpf();
+							
+            wpfGrid.backColor(System.Windows.Media.Brushes.White);				
+            var wpfLabel = wpfGrid.add_Label_Wpf("test 123");
+            var text = wpfLabel.get_Text_Wpf();
+
+            Assert.AreEqual(text, "test 123");
+
+            wpfLabel.set_Text_Wpf("changed");
+            text = wpfLabel.get_Text_Wpf();
+
+            Assert.AreEqual(text, "changed");
+            
+            winFormControl.parentForm().close();
+        }
+    }
+}
