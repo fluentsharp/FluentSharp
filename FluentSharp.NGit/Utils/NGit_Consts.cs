2655649e15589faccb044a0aca035b7627649a26
DinisCruz <dinis.cruz@owasp.org>
2016-05-28 15:06:28 +0000

    removed FluentSharp.NGit files

--- FluentSharp.NGit/Utils/NGit_Consts.cs
+++
@@ -1,9 +0,0 @@
-namespace FluentSharp.Git.Utils
-{
-    public class NGit_Consts
-    {
-        public static string DEFAULT_COMMIT_NAME  = "FluentSharp NGit";
-        public static string DEFAULT_COMMIT_EMAIL = "FluentSharp@o2platform.com";
-        public static string EMPTY_SHA1           = "0000000000000000000000000000000000000000";
-    }    
-}
